0ac2dc19b013613c23e89efd7546568beb71cd21
Harshavardhan <harsha7697@gmail.com>
2023-01-24 20:51:13 +0530

    Added function to check if the IP address is a valid IPv6 address
    @8:51pm
    
    - check if the number of segments is more than 2 and less than or equal
      to 8 because "::" is a valid IPv6 address
    - check if each character within each segment is among the hexadecimal
      characters
    - check if each segment does not have more than 4 characters

--- main.py
+++ main.py
@@ -7,28 +7,58 @@ datefmt='%H:%M:%S %d-%m-%Y ',
 level=logging.INFO
 )
 
-ip_address = "255.255.255.255"
+HEXADECIMALS = "0123456789abcdefABCDEF"
+
+ip_address = "ffff:0:0:0:0:0:0:FFFF"
+# ip_address = ":"
+
 # Function to validate IPv4 addresses
 def is_valid_ipv4(given_ip_addrr) -> bool:
-    logging.debug("Validating if the ip address is ipv4")
+    logging.debug("validating if the ip address is ipv4")
     split_ip_address = given_ip_addrr.split(".")
     logging.debug(split_ip_address)
     if len(split_ip_address) == 4:
-        logging.debug("The given ip address has 4 octets as expected.")
+        logging.debug("the given ip address has 4 octets as expected.")
         if all([int(octet) <= 255 for octet in split_ip_address]):
-            logging.info("Each octet is between 0 - 255")
-            logging.info(f"IP address {given_ip_addrr} is a valid IPv4 address.")
+            logging.info("each octet is between 0 - 255")
+            logging.info(f"ip address {given_ip_addrr} is a valid IPv4 address.")
             return True
         else:
-            logging.warning("The octets do not lie between 0 - 255")
-            logging.warning(f"IP address {given_ip_addrr} is invalid.")
+            logging.warning("the octets do not lie between 0 - 255")
+            logging.warning(f"ip address {given_ip_addrr} is invalid.")
             return False
     else:
-        logging.warning("Number of octets is not equal to 4")
-        logging.warning(f"IP address {given_ip_addrr} is invalid.")
+        logging.warning("number of octets is not equal to 4")
+        logging.warning(f"ip address {given_ip_addrr} is invalid.")
         return False
 
-try:
-    logging.info(is_valid_ipv4(ip_address))
-except:
-    logging.warn("invalid IP")
+
+def is_valid_ipv6(given_ip_addrr) -> bool:
+    logging.debug("validating if the ip address is ipv6")
+    split_ip_address = given_ip_addrr.split(":")
+    logging.debug(split_ip_address)
+    if len(split_ip_address) > 2 and len(split_ip_address) <= 8:
+        logging.debug("ip address has more than 3 and less than 8 segments.")
+        logging.debug([segment[i] for segment in split_ip_address for i in range(len(segment))])
+        logging.debug([segment[i] in HEXADECIMALS for segment in split_ip_address for i in range(len(segment))])
+        if all([segment[i] in HEXADECIMALS for segment in split_ip_address for i in range(len(segment))]):
+            logging.info("each value in each segment is a hexadecimal")
+            if all([len(segment) <= 4 for segment in split_ip_address]):
+                logging.debug("length of each segment is less than 4")
+                logging.info(f"ip address {given_ip_addrr} is a valid ipv6 address")
+                return True
+            else:
+                logging.warning("length of each segment is greater than 4")
+                logging.warning(f"ip address {given_ip_addrr} is invalid")
+                return False
+        else:
+            logging.warning("each value in each segment is not a hexadecimal")
+            logging.warning(f"ip address {given_ip_addrr} is invalid")
+            return False
+    else:
+        logging.warning("ip address has incorrect number segments.")
+        logging.warning(f"ip address {given_ip_addrr} is invalid")
+        return False
+
+
+
