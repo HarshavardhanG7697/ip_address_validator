6a42983c9f04773c41972d36978aa6601b6f2f50
Harshavardhan <harsha7697@gmail.com>
2023-01-24 22:46:34 +0530

    Adding unit tests
    - Renamed the main.py file to ip_addr_validator.py
    - Added basis tests to make sure a fundamental validation rules are not
      impaired

---
+++ test_ip_addr_validator.py
@@ -0,0 +1,18 @@
+import unittest
+import ip_addr_validator
+
+class TestIpAddrValidator(unittest.TestCase):
+    def test_valid_ipv4(self):
+        self.assertTrue(ip_addr_validator.is_valid_ipv4("0.0.0.0"))
+        self.assertFalse(ip_addr_validator.is_valid_ipv4("0.0.0"))
+
+    def test_valid_ipv6(self):
+        self.assertTrue(ip_addr_validator.is_valid_ipv6("::"))
+        self.assertTrue(ip_addr_validator.is_valid_ipv6("0:0:0:0:0:0:0:0"))
+        self.assertFalse(ip_addr_validator.is_valid_ipv6("0:0:0:0:0:0:0:0:0"))
+        self.assertFalse(ip_addr_validator.is_valid_ipv6("0"))
+        self.assertFalse(ip_addr_validator.is_valid_ipv6(":"))
+
+
+if __name__ == "__main__":
+    unittest.main()
