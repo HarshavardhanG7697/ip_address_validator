9e87883ce71d9da224ff9b9bea0b698539a380cc
Harshavardhan <harsha7697@gmail.com>
2023-01-24 22:06:34 +0530

    reformatted the file using black

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/Users/hv/Documents/GitHub/ip_addr_validator/.venv/bin/python3
 import logging
 
-logging.basicConfig(format='%(asctime)s-[%(levelname)s]-%(message)s', 
-datefmt='%H:%M:%S %d-%m-%Y',
-level=logging.INFO
+logging.basicConfig(
+    format="%(asctime)s-[%(levelname)s]-%(message)s",
+    datefmt="%H:%M:%S %d-%m-%Y",
+    level=logging.INFO,
 )
 
 HEXADECIMALS = "0123456789abcdefABCDEF"
@@ -28,16 +29,31 @@ def is_valid_ipv4(given_ip_addrr) -> bool:
         logging.warning(f"ip address {given_ip_addrr} is invalid.")
         return False
 
+
 # Function to validate IPv6 address
 def is_valid_ipv6(given_ip_addrr) -> bool:
     logging.info("validating if the ip address is ipv6")
     split_ip_address = given_ip_addrr.split(":")
-    logging.debug(f"split: {split_ip_address}") 
+    logging.debug(f"split: {split_ip_address}")
     if len(split_ip_address) > 2 and len(split_ip_address) <= 8:
         logging.debug("ip address has more than 3 and less than 8 segments.")
-        logging.debug([segment[i] for segment in split_ip_address for i in range(len(segment))])
-        logging.debug([segment[i] in HEXADECIMALS for segment in split_ip_address for i in range(len(segment))])
-        if all([segment[i] in HEXADECIMALS for segment in split_ip_address for i in range(len(segment))]):
+        logging.debug(
+            [segment[i] for segment in split_ip_address for i in range(len(segment))]
+        )
+        logging.debug(
+            [
+                segment[i] in HEXADECIMALS
+                for segment in split_ip_address
+                for i in range(len(segment))
+            ]
+        )
+        if all(
+            [
+                segment[i] in HEXADECIMALS
+                for segment in split_ip_address
+                for i in range(len(segment))
+            ]
+        ):
             logging.info("each value in each segment is a hexadecimal")
             if all([len(segment) <= 4 for segment in split_ip_address]):
                 logging.debug("length of each segment is less than 4")
