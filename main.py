ba76443e9eaed76e1312ae525b1f166de77d1270
Harshavardhan <harsha7697@gmail.com>
2023-01-24 22:14:18 +0530

    Added comments and docstring

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ HEXADECIMALS = "0123456789abcdefABCDEF"
 
 # Function to validate IPv4 addresses
 def is_valid_ipv4(given_ip_addrr) -> bool:
+    """check if a given ip address is valid IPv4 address
+
+    Args:
+        given_ip_addrr (string): ip address to be validated
+
+    Returns:
+        bool: True if valid IPv4. False if invalid
+    """
     logging.info("validating if the ip address is ipv4")
+    # Split the ip address into octets using . as delimeter
     split_ip_address = given_ip_addrr.split(".")
     logging.debug(f"split: {split_ip_address}")
+    # Confirm that there are 4 octets
     if len(split_ip_address) == 4:
         logging.debug("the given ip address has 4 octets as expected.")
+        # Verify if all the values in octect are between 0 - 255
         if all([int(octet) <= 255 for octet in split_ip_address]):
             logging.info("each octet is between 0 - 255")
             logging.info(f"ip address {given_ip_addrr} is a valid IPv4 address.")
@@ -32,9 +43,19 @@ def is_valid_ipv4(given_ip_addrr) -> bool:
 
 # Function to validate IPv6 address
 def is_valid_ipv6(given_ip_addrr) -> bool:
+    """Check if a given ip address is valid IPv6 address.
+
+    Args:
+        given_ip_addrr (string): ip address to be validated
+
+    Returns:
+        bool: True if valid IPv6. False if invalid
+    """
     logging.info("validating if the ip address is ipv6")
+    # Split the ip address into segments
     split_ip_address = given_ip_addrr.split(":")
     logging.debug(f"split: {split_ip_address}")
+    # Check the number of segments
     if len(split_ip_address) > 2 and len(split_ip_address) <= 8:
         logging.debug("ip address has more than 3 and less than 8 segments.")
         logging.debug(
@@ -47,6 +68,7 @@ def is_valid_ipv6(given_ip_addrr) -> bool:
                 for i in range(len(segment))
             ]
         )
+        # Check if all the characters in segment is a hexadecimal valud
         if all(
             [
                 segment[i] in HEXADECIMALS
@@ -55,6 +77,7 @@ def is_valid_ipv6(given_ip_addrr) -> bool:
             ]
         ):
             logging.info("each value in each segment is a hexadecimal")
+            # Check if each segment is not greater than 4 characters
             if all([len(segment) <= 4 for segment in split_ip_address]):
                 logging.debug("length of each segment is less than 4")
                 logging.info(f"ip address {given_ip_addrr} is a valid ipv6 address")
